2bcf693a489494c1f688c9e60902671285cd31cf
Ana Flávia A. Ferreira <ferreiraanaflaviaalves@gmail.com>
2021-03-30 22:01:24 -0300

    Menu inicial

--- banco/src/main/java/banco/App.java
+++ banco/src/main/java/banco/App.java
@@ -1,13 +1,60 @@
 package banco;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.JOptionPane;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws SQLException
     {
-        System.out.println( "Hello World!" );
+        int op;
+        Scanner leitor = new Scanner(System.in);
+        do{
+            System.out.println( "--Menu--" );
+            System.out.println( "1 - Criar Cliente" );
+            System.out.println( "2 - Criar Conta Corrente" );
+            System.out.println( "3 - Criar Conta Poupança" );
+            System.out.println( "4 - Sacar" );
+            System.out.println( "5 - Depositar" );
+            System.out.println( "6 - Ver Saldo" );
+            System.out.println( "7 - Sair" );
+            System.out.print( "Op: " );
+            op = leitor.nextInt();
+            switch (op) {
+                case 1:
+                    
+                    break;
+    
+                case 2:
+                
+                    break;
+                
+                case 3:
+                    
+                    break;
+    
+                case 4:
+                
+                    break;
+                
+                case 5:
+                    
+                    break;
+    
+                case 6:
+                    
+                    break;
+    
+                case 7:
+                    System.out.println( "7 - Sair" );
+                    break;
+            
+                default:
+                    System.out.println( "Opção inválida." );
+                    break;
+            }
+        }while(op != 7);
     }
 }
